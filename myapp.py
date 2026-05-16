84205025d40257d00bc0eed2f96726d564384fa5
AMD <agrimparwar68@gmail.com>
2026-05-16 18:20:14 +0530

    C6-Added table2 function using while loop

--- myapp.py
+++ myapp.py
@@ -5,3 +5,13 @@ def table():
         print(f"{number} * {i} = {number * i}")
 
 table()
+
+#rewrite table function using while loop
+def table2():
+    number1 = int(input("Enter a number"))
+    i = 1
+    while i <= 12:
+        print(f"{number1} * {i} = {number1 * i}")
+        i+=1
+
+table2()
